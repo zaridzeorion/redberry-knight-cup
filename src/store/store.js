a495aa72074cc8d9991e7a7037f517cc615a4907
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-07-07 22:52:56 +0400

    Redux: import slices into store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import applicant from "./slices/applicantSlice";
+import routesOpenClose from "./slices/routesOpenClose";
+
+const reducer = combineReducers({
+    applicant,
+    routesOpenClose
+});
 
 const store = configureStore({
-    reducer: {}
-})
+    reducer,
+});
 
-export default store
+export default store;
