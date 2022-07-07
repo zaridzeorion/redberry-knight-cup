5f672e1d91001c30f58b64075f4af7e8623eb048
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-07-07 22:41:45 +0400

    Redux: add applicantSlice to save applicant data
    which is needed for last part, where data should be sent to specific API

---
+++ src/store/slices/applicantSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let initialState = {
+    name: "",
+    email: "",
+    phone: "",
+    birthDate: "",
+    levelOfKnowledge: "",
+    character: "",
+    championshipParticipation: "",
+};
+
+const applicantSlice = createSlice({
+    name: "applicant",
+    initialState,
+    reducers: {
+        setName(state, action) {
+            return { ...state, name: action.payload };
+        },
+        setEmail(state, action) {
+            return { ...state, email: action.payload };
+        },
+        setPhone(state, action) {
+            return { ...state, phone: action.payload };
+        },
+        setBirthDate(state, action) {
+            return { ...state, birthDate: action.payload };
+        },
+        setLevelOfKnowledge(state, action) {
+            return { ...state, levelOfKnowledge: action.payload };
+        },
+        setCharacter(state, action) {
+            return { ...state, character: action.payload };
+        },
+        setChampionshipParticipation(state, action) {
+            return { ...state, championshipParticipation: action.payload };
+        },
+    },
+});
+
+export const {
+    setName,
+    setEmail,
+    setPhone,
+    setBirthDate,
+    setLevelOfKnowledge,
+    setCharacter,
+    setChampionshipParticipation
+} = applicantSlice.actions;
+
+export default applicantSlice.reducer;
