8ab4f6c3b196918e3a6f9cdaf9752237e1893896
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-07-08 18:32:30 +0400

    React: define routes for each page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import React from 'react'
 
+import MainPage from './components/MainPage';
+import PersonalInformation from './components/PersonalInformation';
+import Experience from './components/Experience';
+import Completed from './components/Completed';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Redberry Knight Cup</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+        <Route path="/personal-information" element={<PersonalInformation />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/completed" element={<Completed />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
