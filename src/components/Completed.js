54c9aa9de456731117366c8062e9892b0e707812
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-07-08 13:24:26 +0400

    React: add Completed component

---
+++ src/components/Completed.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Completed = () => {
+
+    return (
+        <div>
+            <div>
+                <h2><img alt="Knight" />Redberry Knight Cup</h2>
+
+                <br />
+
+                <img alt="Chess" />
+            </div>
+
+            <div>
+                <img alt="Rocket" /> <br />
+                <h2>Onboarding completed!</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Completed
