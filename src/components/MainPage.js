4d69b8e4547fbb879ec71e9db4d2e7daea981496
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-07-08 12:45:59 +0400

    React: add MainPage component

---
+++ src/components/MainPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const MainPage = () => {
+    return (
+        <div>
+
+            <div>
+                <h2><img alt="Knight" />Redberry Knight Cup</h2>
+
+                <br />
+
+                <img alt="Chess" />
+            </div>
+
+
+            <div>
+                <h1>CHESS SAYS <span>A LOT ABOUT</span> <br /> WHO WE ARE</h1>
+
+                <br />
+
+                <button>Get Started <img alt="Right Arrow" /></button>
+            </div>
+
+        </div>
+    );
+};
+
+export default MainPage;
