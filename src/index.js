780daad15fa08ecd50d43bd766aa5c76e3468551
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-07-09 14:40:40 +0400

    Redux: add feature that saves store after page refresh

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import store from "./store/store"
 import { Provider } from "react-redux"
+
+import { persistor, store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,24 @@ import { combineReducers } from "redux";
 import applicant from "./slices/applicantSlice";
 import routesOpenClose from "./slices/routesOpenClose";
 
+import storage from 'redux-persist/lib/storage';
+
+import { persistReducer, persistStore } from 'redux-persist';
+
 const reducer = combineReducers({
     applicant,
     routesOpenClose
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
 });
 
-const store = configureStore({
-    reducer,
-});
-
-export default store;
+export const persistor = persistStore(store)
