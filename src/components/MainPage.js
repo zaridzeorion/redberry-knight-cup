cbfe52d58d48fdaf5c24d958605cf7189142bcf4
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-07-09 16:29:31 +0400

    React: add style to MainPage

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,29 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import KnightLogo from '../image/KnightLogo.png'
+import Styles from './MainPage.module.css'
 
+import RigthArrow from '../image/RightArrow.png'
 const MainPage = () => {
     return (
-        <div>
+        <div className="Wrapper">
+            <div className="ChessImageMain LeftSide">
 
-            <div>
-                <h2><img alt="Knight" />Redberry Knight Cup</h2>
-
-                <br />
-
-                <img alt="Chess" />
+                <h2 className="LeftHeader">
+                    <span className="LeftHeaderContent Nunito">
+                        <img src={KnightLogo} alt="Knight" />Redberry Knight Cup
+                    </span>
+                </h2>
             </div>
 
 
-            <div>
-                <h1>CHESS SAYS <span>A LOT ABOUT</span> <br /> WHO WE ARE</h1>
-
-                <br />
+            <div className={`Nunito ${Styles.RightSide} RightSide`}>
+                <h1 className={Styles.Title}>CHESS SAYS <span className={Styles.SmallText}>A LOT ABOUT</span> <br /> WHO WE ARE</h1>
 
                 <Link to="personal-information">
-                    <button>Get Started <img alt="Right Arrow" /></button>
+                    <button className={`${Styles.Button} OpenSans`}>
+                        <span className={Styles.GetStarted}>Get Started</span>
+                        <img src={RigthArrow} alt="Right Arrow" />
+                    </button>
                 </Link>
             </div>
-
         </div>
     );
 };
