361c2839b4b6b035abf37c0a16afb79d33f3a0f4
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-07-10 21:57:59 +0400

    React: add right side header visual "active status"

--- src/components/RightHeader.js
+++ src/components/RightHeader.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './RightHeader.module.css'
 
+import { useDispatch, useSelector } from 'react-redux'
+
+import CheckedImage from '../image/Checked.png'
+
 const RightHeader = () => {
+    const dispatch = useDispatch();
+    const applicant = useSelector((state) => state.applicant);
+    const routesOpenClose = useSelector((state) => state.routesOpenClose);
+
+    const [active, setActive] = useState(false)
+
+    useEffect(() => {
+        if (applicant.name || applicant.email || applicant.phone || applicant.birthDate) setActive(true)
+        else setActive(false)
+    }, [applicant])
+
     return (
         <div className={`${styles.RightHeaderWrapper} OpenSans`}>
             <h4 className="RightHeader">
@@ -12,10 +27,18 @@ const RightHeader = () => {
 
             <div className={styles.Wrapper}>
                 <div className={styles.Item1}>
-                    <div className={styles.One}>1</div>
+                    {
+                        routesOpenClose.experience === true ?
+                            <div style={active ? { backgroundColor: '#E9FAF1' } : {}} className={styles.One}>
+                                <img src={CheckedImage} alt="Checked" />
+                            </div>
+                            : <div style={active ? { backgroundColor: '#E9FAF1' } : {}} className={styles.One}>
+                                1
+                            </div>
+                    }
                     <p>Personal information</p>
                 </div>
-                
+
                 <div className={styles.Item2}>
                     <div className={styles.Two}>2</div>
                     <p>Chess experience</p>
